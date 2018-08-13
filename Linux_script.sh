5f24e70bddc334331d1ed9430250f09fa1c32ecb
ceasharks <browa84@gmail.com>
2018-08-13 16:52:27 -0400

    Update Linux_script.sh

--- Linux_script.sh
+++ Linux_script.sh
@@ -25,8 +25,8 @@ machine="
 #▐    ▐            ▐         ▐   ▐    ▐       ▐ ▐         ▐               
 "
 #clears the screen and loggs the shell into root
-echo "clear"
-echo "sudo su"
+clear
+sudo su
 #prints the logo and sets up to reduce errors
 printf "$machine"
 #setup
@@ -37,16 +37,25 @@ echo "unalaias -a" >> /root.bashrc
 
 sleep 5
 #clears the logo and continues to the actual script
-echo "clear"
+clear
 #starting to make functions and displaying setup
 update()
 {
 #updates all programs
-echo "apt-get update; apt-get upgrade"
+	apt-get update
+	apt-get upgrade
 }
 
-enable_ufw()
+gufw_on()
 {
 #installs and enables gufw and ufw
-echo "apt-get install gufw"
-echo "
+	apt-get install gufw
+	ufw enable
+}
+
+pswd_secure()
+{
+#Makes you able to make passwords secure
+	apt-get cracklib
+	
+
