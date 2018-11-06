8fc5cc2f583df379b8d808fe220f5ac1600e3633
ceasharks <browa84@gmail.com>
2018-11-05 21:26:08 -0500

    Update Linux_script.sh

--- Ubuntu/Linux_script.sh
+++ Ubuntu/Linux_script.sh
@@ -105,7 +105,32 @@ install_bum()
 #install bum Boot Up Manager
 	apt-get install bum
 }
-	
+
+delete_MP3()
+{
+cd /home/
+mp3s=$(ls -t *".mp3" | head -n 1)
+
+#menu to select weather to delete or not
+title="Select example"
+prompt="Pick an option:"
+options=("Keep" "Delete" "Examine")
+
+echo "$title"
+PS3="$prompt "
+select opt in "${options[@]}" "Quit"; do 
+
+    case "$REPLY" in
+
+    1 ) echo "You picked $opt which is to Keep the files";;
+    2 ) echo "You picked $opt which is to delete the files";rm *.mp3;;
+    3 ) echo 'you picked $opt which is to look thru all files found"; rm -i *.mp3;;
+
+    * ) echo "Invalid option. Try another one.";continue;;
+
+    esac
+    break
+
 	
 	
 	
