4f40604235d226c3d3885159af78ca4035961d30
ceasharks <browa84@gmail.com>
2018-11-06 14:37:31 -0500

    Update Linux_script.sh

--- Ubuntu/Linux_script.sh
+++ Ubuntu/Linux_script.sh
@@ -55,25 +55,71 @@ tput bel
 sleep 5
 #clears the logo and continues to the actual script
 clear
-#starting to make functions and displaying setup
-update()
-{
-#updates all programs
-	apt-get update
-	apt-get upgrade
-}
 
-gufw_on()
+#starting to make functions and displaying setup
+#################################################################################
+update();									#
+{;										#
+#updates all programs								#
+	apt-get update;								#
+	apt-get upgrade;							#
+};										#
+										#
+install_bum();									#
+{;										#
+#install bum Boot Up Manager							#
+	apt-get install bum;#                  Install block			#
+};										#
+										#
+gufw_on();									#
+{										#
+#installs and enables gufw and ufw						#
+										#
+	apt-get install ufw;							#
+	apt-get install gufw;							#
+	ufw enable;								#
+}                  								#
+#################################################################################
+delete_MP3()
 {
-#installs and enables gufw and ufw
+#2 is the safety block
+cd /home/
+mp3s=$(ls -t *".mp3" | head -n 1)
+#error checking to avoid damage to the system
+	if ["$?" == "0"]; then
+		bell()
+		echo ".Mp3 media files found"
+		sed -i '/(#2)/ D'
+		else
+		#notify user to ensure to fix passwords manually
+		bell()
+		back(1);fore(0);echo "No .Mp3 media files found."
+	fi 
 	
-	apt-get install ufw
-	apt-get install gufw
-	ufw enable
+#menu to select weather to delete or not
+#2 title="files found: $mp3s"
+#2 prompt="Mp3 files have been found pick an option:"
+#2 options=("Keep" "Delete" "Examine")
+#2
+#2 echo "$title"
+#2 PS3="$prompt"
+#2 select opt in "${options[@]}" "Quit"; do 
+
+    #2 case "$REPLY" in
+
+    #2 1 ) echo "You picked $opt which is to Keep the files";;
+    #2 2 ) echo "You picked $opt which is to delete the files";rm *.mp3;;
+    #2 3 ) echo "you picked $opt which is to look thru all files found"; rm -i *.mp3;;
+
+    #2 * ) echo "Invalid option. Try another one.";continue;;
+    
+    #2 esac
+    #2 break
 }
 
 pswd_secure()
 {
+#1 is the safety block
 #Makes passwords secure
 	apt install libpam-cracklib
 	cd /etc/pam.d
@@ -100,38 +146,8 @@ pswd_secure()
 	sed -i '$ a auth required pam_tally2.so deny=5 onerr=fail unlock_time=1800' ./common-auth
 	}
 	
-install_bum()
-{
-#install bum Boot Up Manager
-	apt-get install bum
-}
 
-delete_MP3()
-{
-cd /home/
-mp3s=$(ls -t *".mp3" | head -n 1)
 
-#menu to select weather to delete or not
-title="Select example"
-prompt="Pick an option:"
-options=("Keep" "Delete" "Examine")
-
-echo "$title"
-PS3="$prompt "
-select opt in "${options[@]}" "Quit"; do 
-
-    case "$REPLY" in
-
-    1 ) echo "You picked $opt which is to Keep the files";;
-    2 ) echo "You picked $opt which is to delete the files";rm *.mp3;;
-    3 ) echo 'you picked $opt which is to look thru all files found"; rm -i *.mp3;;
-
-    * ) echo "Invalid option. Try another one.";continue;;
-
-    esac
-    break
-
-	
 	
 	
 	
